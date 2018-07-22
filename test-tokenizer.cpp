ff4647124f246fc4b5ba3c4b98b640f0a4e9f17f
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-21 19:49:36 -0500

    Add failing simple token test.

--- test-tokenizer.cpp
+++ test-tokenizer.cpp
@@ -3,7 +3,8 @@
 #include "tokenizer.h"
 
 void test_simple_token() {
-
+  // Trivial atom parse
+  assert((new MexprBuilder("blah"))->parse()->val.atom->to_str() == "blah");
 }
 
 int main(int argc, char** argv) {
--- tokenizer.h
+++ tokenizer.h
@@ -5,11 +5,14 @@
 
 class MexprBuilder {
 private:
-
   std::string source;
 
 public:
   MexprBuilder(std::string s) : source(s) { }
+
+  MEXP* parse(bool default_parentheses = false) {
+    return new MEXP { { .atom = new MATOM("foo") }, MEXP_TYPE::matom_type };
+  } 
 };
 
 #endif
