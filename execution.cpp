717c42edf1922007ec093417a8324e1e6a7c0990
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-22 10:25:06 -0500

    Move function bodies for strategy classes to cpp file.

--- execution.cpp
+++ execution.cpp
@@ -1,4 +1,55 @@
 #include "execution.h"
 
+/*
+ * LTRTraverseStrategy
+ */
+
+void LTRTraverseStrategy::execute1(MEXP *node, MEXP *sibling) {
+  if (MEXP_IS_ATOM(node)) {
+    effect(node);
+  } else {
+    // Parent -> children tree walk
+    execute1(node->val.node->get_parent());
+    auto children = node->val.node->get_children();
+    for (unsigned int i = 0; i < children->size(); i++) {
+      auto child = children->at(i);
+      auto sibling_node = child;
+      if (i == children->size() - 1) {
+        sibling_node = NULL;
+      } else {
+        sibling_node = children->at(i + 1);
+      }
+      // TODO: Should we pass the full tail here? Or maybe just let do a SIBLING macro?
+      execute1(child, sibling_node);
+    }
+    finalize(node, sibling);
+  }
+}
+
+void LTRTraverseStrategy::execute() {
+  execute1(this->program);
+}
+
+/*
+ * LTRAtomPrinterStrategy
+ */
+
+void LTRAtomPrinterStrategy::execute() {
+  os << "Executing: \n";
+  LTRTraverseStrategy::execute();
+  os << "\nExecution completed.\n";
+}
+
+void LTRAtomPrinterStrategy::effect(MEXP *node) {
+  if (MEXP_IS_ATOM(node)) {
+    os << "Atom: " << MEXP_TO_STR(node) << std::endl;
+  }
+}
+
+void LTRAtomPrinterStrategy::finalize(MEXP *node, MEXP *sibling) {
+  if (MEXP_IS_EXPR(node)) {
+    os << "Finalizing: " << MEXP_TO_STR(node->val.node->get_parent()) << std::endl;
+  }
+}
 
 
--- execution.h
+++ execution.h
@@ -20,34 +20,12 @@ public:
 class LTRTraverseStrategy : public MispExecutionStrategy {
 protected:
 
-  void execute1(MEXP *node, MEXP *sibling = NULL) {
-    if (MEXP_IS_ATOM(node)) {
-      effect(node);
-    } else {
-      // Parent -> children tree walk
-      execute1(node->val.node->get_parent());
-      auto children = node->val.node->get_children();
-      for (unsigned int i = 0; i < children->size(); i++) {
-        auto child = children->at(i);
-        auto sibling_node = child;
-        if (i == children->size() - 1) {
-          sibling_node = NULL;
-        } else {
-          sibling_node = children->at(i + 1);
-        }
-        // TODO: Should we pass the full tail here? Or maybe just let do a SIBLING macro?
-        execute1(child, sibling_node);
-      }
-      finalize(node, sibling);
-    }
-  }
+  void execute1(MEXP *node, MEXP *sibling = NULL);
 
 public:
   LTRTraverseStrategy(MEXP *_program) : MispExecutionStrategy(_program) { }
 
-  void execute() {
-    execute1(this->program);
-  }
+  void execute();
 };
 
 class LTRAtomPrinterStrategy : public LTRTraverseStrategy {
@@ -57,23 +35,9 @@ private:
 public:
   LTRAtomPrinterStrategy(MEXP *_program, std::ostream &_os = std::cout) : LTRTraverseStrategy(_program), os(_os) { }
 
-  void execute() {
-    os << "Executing: \n";
-    LTRTraverseStrategy::execute();
-    os << "\nExecution completed.\n";
-  }
-
-  void effect(MEXP *node) {
-    if (MEXP_IS_ATOM(node)) {
-      os << "Atom: " << MEXP_TO_STR(node) << std::endl;
-    }
-  }
-
-  void finalize(MEXP *node, MEXP *sibling = NULL) {
-    if (MEXP_IS_EXPR(node)) {
-      os << "Finalizing: " << MEXP_TO_STR(node->val.node->get_parent()) << std::endl;
-    }
-  }
+  void execute();
+  void effect(MEXP *node);
+  void finalize(MEXP *node, MEXP *sibling = NULL);
 };
 
 #endif
