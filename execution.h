5b1e65ea06984f61d8d0cf382f5c0142cdc75f22
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-22 10:17:20 -0500

    Complete working LTR printer strategy.

--- execution.h
+++ execution.h
@@ -14,56 +14,65 @@ public:
 
   virtual void execute() = 0;
   virtual void effect(MEXP *node) = 0;
+  virtual void finalize(MEXP *node, MEXP *sibling = NULL) = 0;
 };
 
 class LTRTraverseStrategy : public MispExecutionStrategy {
 protected:
 
-  virtual void execute1(MEXP *tree, MEXP *sibling = NULL) = 0;
+  void execute1(MEXP *node, MEXP *sibling = NULL) {
+    if (MEXP_IS_ATOM(node)) {
+      effect(node);
+    } else {
+      // Parent -> children tree walk
+      execute1(node->val.node->get_parent());
+      auto children = node->val.node->get_children();
+      for (unsigned int i = 0; i < children->size(); i++) {
+        auto child = children->at(i);
+        auto sibling_node = child;
+        if (i == children->size() - 1) {
+          sibling_node = NULL;
+        } else {
+          sibling_node = children->at(i + 1);
+        }
+        // TODO: Should we pass the full tail here? Or maybe just let do a SIBLING macro?
+        execute1(child, sibling_node);
+      }
+      finalize(node, sibling);
+    }
+  }
 
 public:
   LTRTraverseStrategy(MEXP *_program) : MispExecutionStrategy(_program) { }
 
   void execute() {
-    if (MEXP_IS_ATOM(this->program)) {
-      execute1(this->program);
-    } else {
-      // Parent -> children tree walk
-      execute1(this->program->val.node->get_parent());
-      auto children = this->program->val.node->get_children();
-      for (unsigned int i = 0; i < children->size(); i++) {
-        auto child = children->at(i);
-        auto sibling = child;
-        if (i == children->size() - 1) {
-          sibling = NULL;
-        } else {
-          sibling = children->at(i + 1);
-        }
-        // TODO: Should we pass the full tail here? Or maybe just let do a SIBLING macro?
-        execute1(child, sibling);
-      }
-    }
+    execute1(this->program);
   }
 };
 
 class LTRAtomPrinterStrategy : public LTRTraverseStrategy {
-protected:
-  void execute1(MEXP *tree, MEXP *sibling = NULL) {
-    if (MEXP_IS_ATOM(tree)) {
-      effect(tree);
-    }
-  }
+private:
+  std::ostream &os;
 
 public:
+  LTRAtomPrinterStrategy(MEXP *_program, std::ostream &_os = std::cout) : LTRTraverseStrategy(_program), os(_os) { }
 
   void execute() {
-    std::cout << "Executing: \n";
+    os << "Executing: \n";
     LTRTraverseStrategy::execute();
-    std::cout << "\nExecution completed.\n";
+    os << "\nExecution completed.\n";
   }
 
   void effect(MEXP *node) {
-    std::cout << "Atom: " << MEXP_TO_STR(node) << std::endl;
+    if (MEXP_IS_ATOM(node)) {
+      os << "Atom: " << MEXP_TO_STR(node) << std::endl;
+    }
+  }
+
+  void finalize(MEXP *node, MEXP *sibling = NULL) {
+    if (MEXP_IS_EXPR(node)) {
+      os << "Finalizing: " << MEXP_TO_STR(node->val.node->get_parent()) << std::endl;
+    }
   }
 };
 
--- test-execution.cpp
+++ test-execution.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include <assert.h>
 #include "execution.h"
 
 void test_trivial_execution() {
   // Fill in
+  std::stringstream ss;
+  MEXP *program = (new MexprBuilder("foo [bar baz] bux"))->parse();
+  LTRAtomPrinterStrategy(program, ss).execute();
+  std::cout << ss.str();
 }
 
 int main(int argc, char** argv) {
