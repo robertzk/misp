f4d5ae332e8a450ab324e9981660982622f2cbbc
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-22 17:43:52 -0500

    Fix spacing in ShellExecutionBinding::shell_exec

--- strategies.cpp
+++ strategies.cpp
@@ -69,7 +69,9 @@ std::string ShellExecutionBinding::shell_exec(std::string cmd) {
 	std::array<char, 128> buffer;
 	std::string result;
 	std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
-	if (!pipe) throw std::runtime_error("popen() failed!");
+	if (!pipe) {
+	  throw std::runtime_error("popen() failed!");
+	}
 	while (!feof(pipe.get())) {
 		if (fgets(buffer.data(), 128, pipe.get()) != nullptr) {
 			result += buffer.data();
