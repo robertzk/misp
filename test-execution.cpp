5d364ce249dbd7e94bff26acdfd102eb71c60e34
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-22 10:25:15 -0500

    Remove stale comment.

--- test-execution.cpp
+++ test-execution.cpp
@@ -9,7 +9,6 @@ void assert_contains(std::string s, std::string sub) {
 }
 
 void test_trivial_execution() {
-  // Fill in
   std::stringstream ss;
   MEXP *program = (new MexprBuilder("foo [bar baz] bux"))->parse();
   LTRAtomPrinterStrategy(program, ss).execute();
