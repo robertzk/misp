5070f762eee238bc456bf850136e9cc62eb72403
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-22 10:20:55 -0500

    Complete working LTR printer strategy.

--- test-execution.cpp
+++ test-execution.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <sstream>
 #include <assert.h>
+#include <string>
 #include "execution.h"
+
+void assert_contains(std::string s, std::string sub) {
+  assert(s.find(sub) != std::string::npos);
+}
 
 void test_trivial_execution() {
   // Fill in
   std::stringstream ss;
   MEXP *program = (new MexprBuilder("foo [bar baz] bux"))->parse();
   LTRAtomPrinterStrategy(program, ss).execute();
-  std::cout << ss.str();
+  std::string s = ss.str();
+  char *substrings[] = {"Atom: (", "Atom: [", "Atom: bar", "Atom: baz", 
+											  "Finalizing: [", "Atom: bux", "Finalizing: ("};
+	for (unsigned int i = 0; i < 7; i++) {
+    assert_contains(s, std::string(substrings[i]));
+	}
 }
 
 int main(int argc, char** argv) {
