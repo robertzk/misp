61ead32bdadcb16af5edc21305429cc6879c46d8
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-07-21 21:28:04 -0500

    Test for simple list parsing.

--- test-tokenizer.cpp
+++ test-tokenizer.cpp
@@ -11,8 +11,20 @@ void test_atom() {
   assert(MEXP_TO_STR(el2) == "(blah)");
 }
 
+void test_simple_list() {
+  MEXP *el  = (new MexprBuilder("foo bar baz"))->parse();
+  assert(MEXP_TO_STR(el) == "(foo bar baz)");
+
+  MEXP *el2 = (new MexprBuilder("  {[  foo  bar  baz  ]}  "))->parse(false);
+  assert(MEXP_TO_STR(el2) == "{[ foo bar baz ]}");
+
+  MEXP *el3 = (new MexprBuilder("<:< hey  there >:>"))->parse(false);
+  assert(MEXP_TO_STR(el3) == "<:< hey there >:>");
+}
+
 int main(int argc, char** argv) {
   test_atom();
+  test_simple_list();
 
   return 0;
 }
